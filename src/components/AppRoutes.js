775db4de0d2212f9c9a76a187c70e1932f87462d
ArtyomKosakyan <artyomkosakyan@outlook.com>
2021-05-03 15:23:18 +0400

    redux integrated worked front-end for Driver

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -21,67 +21,68 @@ import {ChangeActionLoading,
         ChangeActionLoggedIn} from '../redux/actions'
 
 const AppRoutes = (props) => {
-    const location = useLocation();
-    
-    useEffect(() => {
-        userInChecker()
-          .then()
-    },[])
-    
-    const userInChecker = async () => {
-        await userIn()
-          .then(response => {
-            loadingDone()
-            if (response.data.user_in) {
-              console.log(response.data)
-              loginStatusChanger({
-                'isLoading': false,
-                'loggedIn': response.data.user_in,
-                'userType': response.data.model_name,
-                'userId': response.data.user.id} )
-              console.log(props.appState , 'AppRoutes useEffect')
-              console.log(response.data.user_in , 'AppRoutes useEffect')
-            }
-            else {
-              loginStatusChanger({
-                'isLoading': false,
-                'loggedIn': false,
-                'userType': '',
-                'userId': ''} )
-            }
+  const location = useLocation();
+  
+  useEffect(() => {
+    userInChecker()
+      .then()
+  }, [])
+  
+  const userInChecker = async () => {
+    await userIn()
+      .then(response => {
+        loadingDone()
+        if (response.data.user_in) {
+          loginStatusChanger({
+            'isLoading': false,
+            'loggedIn': response.data.user_in,
+            'userType': response.data.model_name,
+            'userId': response.data.user.id
           })
-      
-    }
+        } else {
+          loginStatusChanger({
+            'isLoading': false,
+            'loggedIn': false,
+            'userType': '',
+            'userId': ''
+          })
+        }
+      })
     
-    const loginStatusChanger = (data) => {
-      console.log(data, 'loginStatusChanger')
-      props.dispatch(ChangeActionLoggedIn(data))
-    }
-    const loadingDone = () => {
-      props.dispatch(ChangeActionLoading(false))
-    }
+  }
+  
+  const loginStatusChanger = (data) => {
+    props.dispatch(ChangeActionLoggedIn(data))
+  }
+  const loadingDone = () => {
+    props.dispatch(ChangeActionLoading(false))
+  }
+  
+  return (
     
-    return (
-      
+    <div>
+      {props.appState.isLoading ? <Loading/> :
         <div>
-          {props.appState.isLoading ? <Loading /> : <Header />}
-            <Switch >
-                <Route path={appRoutes.signup} exact component={Signup} />
-                <Route path={appRoutes.login} exact component={Login} />
-                <Route path={appRoutes.signupClient} exact component={ClientSignup} />
-                <Route path={appRoutes.signupDriver} exact component={DriverSignup} />
-                <Route path={appRoutes.loginDriver} exact component={DriverLogin} />
-                <Route path={appRoutes.loginClient} exact component={ClientLogin} />
-                <Route path={appRoutes.loginAdmin} exact component={AdminLogin} />
-                <Route path={appRoutes.client} exact component={ClientOrder} />
-                <Route path={appRoutes.clientHistory} exact component={ClientHistory} />
-                <Route path={appRoutes.clientSettings} exact component={ClientSettings} />
-                <Route path={appRoutes.taxi} exact component={Taxi} />
-                <Route path={appRoutes.driverPage} exact component={DriverPage} />
-                <Route path={appRoutes.driverProfile} exact component={DriverProfile} />
-            </Switch>
+          <Header/>
+          <Switch>
+            <Route path={appRoutes.signup} exact component={Signup}/>
+            <Route path={appRoutes.login} exact component={Login}/>
+            <Route path={appRoutes.signupClient} exact component={ClientSignup}/>
+            <Route path={appRoutes.signupDriver} exact component={DriverSignup}/>
+            <Route path={appRoutes.loginDriver} exact component={DriverLogin}/>
+            <Route path={appRoutes.loginClient} exact component={ClientLogin}/>
+            <Route path={appRoutes.loginAdmin} exact component={AdminLogin}/>
+            <Route path={appRoutes.client} exact component={ClientOrder}/>
+            <Route path={appRoutes.clientHistory} exact component={ClientHistory}/>
+            <Route path={appRoutes.clientSettings} exact component={ClientSettings}/>
+            <Route path={appRoutes.taxi} exact component={Taxi}/>
+            <Route path={appRoutes.driverPage} exact component={DriverPage}/>
+            <Route path={appRoutes.driverProfile} exact component={DriverProfile}/>
+          </Switch>
         </div>
-    )
+      }
+    </div>
+  )
 }
 
 export default AppRoutes
