4f2a7f16ae4fb6f86459014d2cc10c8554b3606d
sevak-yer <yeressian@gmail.com>
2021-05-11 13:17:42 +0400

    clientOrder.js updated

--- src/components/Client/ClientOrder.js
+++ src/components/Client/ClientOrder.js
@@ -7,54 +7,56 @@ import { mapStateToProps } from '../../redux/actions';
 import { createCarType } from "../../redux/actions";
 
 const ClientPage = (props) => {
-    const state = props.appState
-    const dispatch = props.dispatch
-    const history = useHistory();
-    const menuItems = ClientMenu();
+  const state = props.appState
+  const dispatch = props.dispatch
+  const history = useHistory();
+  const menuItems = ClientMenu();
 
-    const handleOrders = (order) => {
-        dispatch(createCarType({
-            ...state,
-            'type': order
-        }))
-        history.push(appRoutes.taxi)
-    }
+  const handleOrders = (order) => {
+    dispatch(createCarType({
+      ...state,
+      'type': order
+    }))
+    history.push(appRoutes.taxi)
+  }
 
-    const orderButton = orderTypes.map((order) => {
-        return (
-            <div key={order}>
-                <button onClick={() => handleOrders(order)} className="btn btn-outline-success mb-1 w-50" type="submit">{order}</button>
-                <br />
-            </div>
-        )
-    })
-      .then((response) => response)
-      .catch((error) => error);
-    history.push(appRoutes.taxi);
-  // };
-
-  // const orderButton = orderTypes.map((order) => (
-  //   <PageButton
-  //     button={order[0]}
-  //     onSelect={handleOrders}
-  //     buttonClassName="grid"
-  //     className={order[1]}
-  //   />
-  // ));
+  const orderButton = orderTypes.map((order) => {
+    return (
+      <div key={order}>
+        <button onClick={() => handleOrders(order)} className="btn btn-outline-success mb-1 w-50" type="submit">{order}</button>
+        <br />
+      </div>
+    )
+  })
 
   return (
-    <div>
-      <div className="menu">
-        <ul>
-          <ClientMenu />
+    <div className="position-relative">
+      <div className="card text-center position-absolute top-0 start-0 ms-4 mt-3" style={{ width: "150px", height: "125px" }}>
+        <ul className="list-group list-group-flush">
+          {menuItems}
         </ul>
       </div>
-      <div className="ui-component container-md">
+
+      <div className="card text-center border position-fixed top-50 start-50 translate-middle" style={{ width: "300px", height: "280px" }}>
         <h5 className="mt-3 mb-3">Choose Vehicle Type</h5>
         {orderButton}
       </div>
     </div>
-  );
+  )
+
+  //   return (
+  //     <div>
+  //       <div className="menu">
+  //         <ul>
+  //           <ClientMenu />
+  //         </ul>
+  //       </div>
+  //       <div className="ui-component container-md">
+  //         <h5 className="mt-3 mb-3">Choose Vehicle Type</h5>
+  //         {orderButton}
+  //       </div>
+  //     </div>
+  //   );
 };
 
 export default connect(mapStateToProps)(ClientPage);
