db0ab4b307213ceddc173e2a2afecba38871ee10
ArtyomKosakyan <artyomkosakyan@outlook.com>
2021-05-13 08:29:08 +0400

    refactored ClientField component

--- src/components/Admin/ClientField.js
+++ src/components/Admin/ClientField.js
@@ -2,81 +2,36 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { mapStateToProps } from '../../redux/actions';
+import ClientFieldDriver from './ClientFieldDriver';
+import ClientFieldPassenger from './ClientFieldPassenger';
+import ClientFieldDefault from './ClientFieldDefault';
 
 const ClientField = ({ usersList, client }) => {
   let fields;
   switch (client) {
     case 'passengers':
       try {
-        fields = usersList.users.map(({ passenger }) => {
-          const {
-            id, phoneNumber, firstName, lastName,
-          } = passenger;
-          return (
-            <div key={id} className="list-group-item list-group-item-action ">
-              <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">
-                  { `${firstName} ${lastName}` }
-                </h5>
-                <small> </small>
-              </div>
-              <p className="mb-1">
-                { `Phone number ${phoneNumber}`}
-              </p>
-            </div>
-          );
-        });
+        fields = usersList.users.map(({ passenger }) => (
+          <ClientFieldPassenger key={passenger.id} passenger={passenger} />
+        ));
       } catch (err) {
         console.log(err);
       }
       break;
     case 'drivers':
       try {
-        fields = usersList.users.map(({ driver }) => {
-          const {
-            id, phoneNumber, firstName, lastName, isActive, isVerifiedByAdmin,
-          } = driver;
-          let verified = 'bg-danger';
-          let active = 'bg-danger';
-          if (isVerifiedByAdmin) {
-            verified = 'bg-success';
-            if (isActive) {
-              active = 'bg-success';
-            }
-          }
-          return (
-            <div key={id} className="list-group-item list-group-item-action">
-              <div className="d-flex w-100 justify-content-between align-items-center">
-                <h5 className="mb-1">
-                  { `${firstName} ${lastName}` }
-                </h5>
-                <span className={`badge ${verified}`}>
-                  {isVerifiedByAdmin ? 'Confirmed' : 'Unconfirmed'}
-                </span>
-              </div>
-              <p className="mb-1">
-                { `Phone number ${phoneNumber}`}
-              </p>
-              <small className={`badge ${active}`}>
-                {isActive ? 'Active' : 'Inactive'}
-              </small>
-            </div>
-          );
-        });
+        fields = usersList.users.map(({ driver }) => (
+          <ClientFieldDriver
+            key={driver.id}
+            driver={driver}
+          />
+        ));
       } catch (err) {
         console.log(err);
       }
       break;
     default:
-      fields = [false].map(() => (
-        <div key={0} className="list-group-item list-group-item-action ">
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">
-              No people
-            </h5>
-          </div>
-        </div>
-      ));
+      fields = [false].map(() => <ClientFieldDefault />);
   }
 
   return (
---
+++ src/components/Admin/ClientFieldPassenger.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ClientFieldPassenger = ({ passenger }) => {
+  const {
+    id, phoneNumber, firstName, lastName,
+  } = passenger;
+  return (
+    <div key={id} className="list-group-item list-group-item-action ">
+      <div className="d-flex w-100 justify-content-between">
+        <h5 className="mb-1">
+          { `${firstName} ${lastName}` }
+        </h5>
+        <small> </small>
+      </div>
+      <p className="mb-1">
+        { `Phone number ${phoneNumber}`}
+      </p>
+    </div>
+  );
+};
+
+ClientFieldPassenger.propTypes = {
+  passenger: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default ClientFieldPassenger;
