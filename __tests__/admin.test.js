071a3b3fa65cefac6371a5682477353119b8752c
ArtyomKosakyan <artyomkosakyan@outlook.com>
2021-05-17 21:17:56 +0400

    added any unit-tests for admin when he is logged in

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -3,8 +3,11 @@ import { configure, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallowToJson } from 'enzyme-to-json';
 import Login from '../src/components/Login';
-import AdminLogin from '../src/components/Admin/AdminLogin';
-import { logout } from '../src/utils/API';
+import { login, logout } from '../src/utils/API';
+import { loginParams } from '../src/utils/configs';
+import ClientFieldPassenger from '../src/components/Admin/ClientFieldPassenger';
+import ClientFieldDriver from '../src/components/Admin/ClientFieldDriver';
+import ClientFieldDefault from '../src/components/Admin/ClientFieldDefault';
 
 describe('Admin testing sets (loggedOut)', () => {
   beforeAll(() => {
@@ -13,10 +16,48 @@ describe('Admin testing sets (loggedOut)', () => {
   configure({ adapter: new Adapter() });
   it('Render correctly Login', () => {
     const component = render(<Login />);
+
     expect(shallowToJson(component)).toMatchSnapshot();
   });
-  it('Render correctly Login admin', () => {
-    const component = render(<AdminLogin />);
+});
+
+describe('Admin testing sets (loggedIn)', () => {
+  beforeAll(() => {
+    const params = loginParams;
+    params.phone_number = '077808489';
+    params.password = '12345678';
+    params.model_name = 'SuperUser';
+
+    login(params);
+  });
+  configure({ adapter: new Adapter() });
+
+  it('Render correctly ClientFieldPassenger', () => {
+    const component = render(<ClientFieldPassenger passenger={{
+      id: 1, phoneNumber: '094808489', firstName: 'Artyom', lastName: 'Kosakyan',
+    }}
+    />);
+
+    expect(shallowToJson(component)).toMatchSnapshot();
+  });
+
+  it('Render correctly ClientFieldDriver', () => {
+    const component = render(<ClientFieldDriver passenger={{
+      id: 1,
+      phoneNumber: '094808489',
+      firstName: 'Artyom',
+      lastName: 'Kosakyan',
+      isActive: false,
+      isVerifiedByAdmin: false,
+    }}
+    />);
+
+    expect(shallowToJson(component)).toMatchSnapshot();
+  });
+
+  it('Render correctly ClientFieldDefault', () => {
+    const component = render(<ClientFieldDefault />);
+
     expect(shallowToJson(component)).toMatchSnapshot();
   });
 });
