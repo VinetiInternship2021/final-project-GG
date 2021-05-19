c40a779e41322fb3594a203c6a6c46ad7a805bb6
ArtyomKosakyan <artyomkosakyan@outlook.com>
2021-05-19 08:03:33 +0400

    deleted api test sets

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -3,60 +3,39 @@ import { configure, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallowToJson } from 'enzyme-to-json';
 import Login from '../src/components/Login';
-import { login, logout } from '../src/utils/API';
-import { loginParams } from '../src/utils/configs';
-import ClientFieldPassenger from '../src/components/Admin/ClientFieldPassenger';
 import ClientFieldDriver from '../src/components/Admin/ClientFieldDriver';
-import ClientFieldDefault from '../src/components/Admin/ClientFieldDefault';
+import ClientFieldPassenger from '../src/components/Admin/ClientFieldPassenger';
 
 describe('Admin testing sets (loggedOut)', () => {
-  beforeAll(() => {
-    logout();
-  });
   configure({ adapter: new Adapter() });
   it('Render correctly Login', () => {
     const component = render(<Login />);
 
     expect(shallowToJson(component)).toMatchSnapshot();
   });
-});
 
-describe('Admin testing sets (loggedIn)', () => {
-  beforeAll(() => {
-    const params = loginParams;
-    params.phone_number = '077808489';
-    params.password = '12345678';
-    params.model_name = 'SuperUser';
-
-    login(params);
-  });
-  configure({ adapter: new Adapter() });
-
-  it('Render correctly ClientFieldPassenger', () => {
-    const component = render(<ClientFieldPassenger passenger={{
-      id: 1, phoneNumber: '094808489', firstName: 'Artyom', lastName: 'Kosakyan',
-    }}
-    />);
-
-    expect(shallowToJson(component)).toMatchSnapshot();
-  });
-
-  it('Render correctly ClientFieldDriver', () => {
-    const component = render(<ClientFieldDriver passenger={{
+  it('Render ClientFieldDriver correctly', () => {
+    const driver = {
       id: 1,
       phoneNumber: '094808489',
       firstName: 'Artyom',
       lastName: 'Kosakyan',
       isActive: false,
       isVerifiedByAdmin: false,
-    }}
-    />);
+    };
+    const component = render(<ClientFieldDriver driver={driver} />);
 
     expect(shallowToJson(component)).toMatchSnapshot();
   });
 
-  it('Render correctly ClientFieldDefault', () => {
-    const component = render(<ClientFieldDefault />);
+  it('Render ClientFieldPassenger correctly', () => {
+    const passenger = {
+      id: 1,
+      phoneNumber: '094808489',
+      firstName: 'Artyom',
+      lastName: 'Kosakyan',
+    };
+    const component = render(<ClientFieldPassenger passenger={passenger} />);
 
     expect(shallowToJson(component)).toMatchSnapshot();
   });
