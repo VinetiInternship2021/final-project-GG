aef59231c4b2d0812470bb41a870b1d6fcbbce61
iwazowsky <hvazgen@gmail.com>
2021-05-19 11:11:50 +0400

    added variable separations

--- cypress/cypress/integration/tests/login.test.js
+++ cypress/cypress/integration/tests/login.test.js
@@ -11,54 +11,63 @@ describe('Passenger Login', () => {
 
   it('Logins as passenger without remember me', () => {
     const { phoneNumber, password } = passenger;
+
     cy.loginAsPassenger(phoneNumber, password);
     cy.loginSucceed();
   });
 
   it('Logins as admin approved driver without remember me', () => {
     const { phoneNumber, password } = driver;
+
     cy.loginAsDriver(phoneNumber, password);
     cy.loginSucceed();
   });
 
   it('Logins as passenger with unexisting phone number', () => {
     const { password } = passenger;
+
     cy.loginAsPassenger('+37400200200', password);
     cy.loginFailed();
   });
 
   it('Logins as passenger with wrong password', () => {
     const { phoneNumber } = passenger;
+
     cy.loginAsPassenger(phoneNumber, 'wrongPassword');
     cy.loginFailed();
   });
 
   it('Logins as admin approved driver with unexisting phone number', () => {
     const { password } = driver;
+
     cy.loginAsPassenger('+37400200200', password);
     cy.loginFailed();
   });
 
   it('Logins as admin approved driver with wrong password', () => {
     const { phoneNumber } = driver;
+
     cy.loginAsPassenger(phoneNumber, 'wrongPassword');
     cy.loginFailed();
   });
 
   it('Logins as passenger in driver page', () => {
     const { phoneNumber, password } = passenger;
+
     cy.loginAsPassenger(phoneNumber, password);
     cy.loginFailed();
   });
 
   it('Logins as passenger with remember me', () => {
     const { phoneNumber, password } = passenger;
+
     cy.loginAsPassenger(phoneNumber, password, true);
     cy.loginSucceed();
   });
 
   it('Logins as admin approved driver witх remember me', () => {
     const { phoneNumber, password } = driver;
+
     cy.loginAsDriver(phoneNumber, password, true);
     cy.loginSucceed();
   });
