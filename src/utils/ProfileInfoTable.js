edcec38df37ea26116833ad4bffc67ee664c40ad
ArtyomKosakyan <artyomkosakyan@outlook.com>
2021-05-26 07:50:16 +0400

    solved profile information styles problem

--- src/utils/ProfileInfoTable.js
+++ src/utils/ProfileInfoTable.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ProfileInfoTable = ({ fieldsData }) => {
   const fields = Object.entries(fieldsData).map(([key, value]) => (
     (key !== 'driver_license_image_id' && key !== 'reservations')
     && (
-    <table key={key} className="table table-borderless">
-      <thead>
-        <tr>
-          <th>{ key }</th>
-          <th>{typeof value === 'boolean' ? value.toString() : value}</th>
-        </tr>
-      </thead>
-    </table>
+      <tr key={key}>
+        <th>{ key }</th>
+        <th>{typeof value === 'boolean' ? value.toString() : value}</th>
+      </tr>
     )
   ));
 
-  return fields;
+  return (
+    <table className="table table-borderless">
+      <tbody>
+        {fields}
+      </tbody>
+    </table>
+  );
+};
+
+ProfileInfoTable.defaultProps = {
+  fieldsData: { field: 'Null' },
+};
+
+ProfileInfoTable.propTypes = {
+  fieldsData: PropTypes.objectOf(PropTypes.any),
 };
 
 export default ProfileInfoTable;
